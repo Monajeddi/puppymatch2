c2c74e27d525c9b6b23d9af9793535ffeaf0fb31
Monajeddi <76617488+Monajeddi@users.noreply.github.com>
2021-04-29 17:47:19 +0200

    Update server.js

--- server.js
+++ server.js
@@ -23,13 +23,13 @@ app.use('/api/annonces',require('./routes/annonce'));
 app.use('/api/user',require('./routes/user'))
 
 //Serve static assets if in production 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
-    //set static folder
-    app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-    });
-  }
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static('client/build'))
+    const path=require('path')
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client', 'build' , 'index.html'))
+    })
+}
 
 //port 
 const PORT = process.env.PORT 
